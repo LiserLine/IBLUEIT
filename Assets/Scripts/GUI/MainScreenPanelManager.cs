534ec0e410a3f780d6ec17789c8ffa713f81ed44
Renato Grimes <rengrimes@gmail.com>
2017-08-17 18:52:51 -0300

    Implementação basica de GUI

---
+++ Assets/Scripts/GUI/MainScreenPanelManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class MainScreenPanelManager : MonoBehaviour
+{
+    public GameObject Panel1, Panel2;
+
+    public void ButtonStartGame()
+    {
+        Panel1.SetActive(false);
+        Panel2.SetActive(true);
+    }
+
+    public void ButtonNewGame()
+    {
+
+    }
+
+    public void ButtonLoadGame()
+    {
+
+    }
+}
