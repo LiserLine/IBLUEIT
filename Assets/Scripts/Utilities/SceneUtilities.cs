9d42c3c324f6055fef2166db0c972a2ef47fd2ed
Renato Grimes <rengrimes@gmail.com>
2017-08-17 16:54:20 -0300

    SceneController alterado para SceneUtilities

---
+++ Assets/Scripts/Utilities/SceneUtilities.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneUtilities : MonoBehaviour
+{
+    private void Update()
+    {
+        OnEscape();
+    }
+
+    private void OnEscape()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
