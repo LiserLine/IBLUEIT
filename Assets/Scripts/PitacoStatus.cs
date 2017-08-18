b5ba3bc346b5787f965c18f352112c866d9a6360
Renato Grimes <rengrimes@gmail.com>
2017-08-18 16:49:08 -0300

    Icone de Status do PITACO

---
+++ Assets/Scripts/PitacoStatus.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PitacoStatus : MonoBehaviour
+{
+    public Sprite Disconnected;
+    public Sprite Connected;
+
+    private SerialController _serialController;
+    private Image _image;
+
+    void Start()
+    {
+        _serialController = GameObject.Find("SerialController").GetComponent<SerialController>();
+        _image = GetComponent<Image>();
+    }
+
+    void Update()
+    {
+        var message = _serialController.ReadSerialMessage();
+        if (message == null)
+            return;
+
+        if (ReferenceEquals(message, SerialController.SERIAL_DEVICE_CONNECTED))
+            _image.sprite = Connected;
+        else if (ReferenceEquals(message, SerialController.SERIAL_DEVICE_DISCONNECTED))
+            _image.sprite = Disconnected;
+    }
+}
