7f9058ee56e3abe83672945a4c3033d2e48222e2
Renato Grimes <rengrimes@gmail.com>
2017-08-21 17:17:18 -0300

    Pasta savedata padronizado por OS

--- Assets/Scripts/Utilities/GameConstants.cs
+++ Assets/Scripts/Utilities/GameConstants.cs
@@ -3,8 +3,16 @@ using UnityEngine;
 
 public class GameConstants
 {
+#if UNITY_ANDROID
     public static readonly string SaveDataPath = Application.persistentDataPath + Path.AltDirectorySeparatorChar + "savedata";
+#else
+    public static readonly string SaveDataPath = "savedata";
+#endif
+
     public static readonly string LocalizationPath = SaveDataPath + Path.AltDirectorySeparatorChar + "localization.dat";
+    public static readonly string PacientsPath = SaveDataPath + Path.AltDirectorySeparatorChar + "pacients";
+
+    public static readonly string SummaryCsvPath = PacientsPath + Path.AltDirectorySeparatorChar + "_summary.csv";
 }
 
 public enum ControlBehaviour
