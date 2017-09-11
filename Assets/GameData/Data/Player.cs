b6342b8292b4f0db7242c28ee99b07bef4e24f42
Renato Grimes <rengrimes@gmail.com>
2017-09-11 18:30:45 -0300

    Dados do paciente dependem do PowerMercy

--- Assets/GameData/Data/Player.cs
+++ Assets/GameData/Data/Player.cs
@@ -2,20 +2,56 @@
 
 public class Player
 {
+    private float _ipf, _epf, _ift, _eft, _rf;
+
     public uint Id { get; set; }
+
     public string Name { get; set; }
+
     public DateTime Birthday { get; set; }
+
     public string Observations { get; set; }
+
     public Disfunctions Disfunction { get; set; }
-    public float InspiratoryPeakFlow { get; set; }
-    public float ExpiratoryPeakFlow { get; set; }
-    public float InspiratoryFlowTime { get; set; }
-    public float ExpiratoryFlowTime { get; set; }
-    public float RespirationFrequency { get; set; }
+
+    public float InspiratoryPeakFlow
+    {
+        get { return _ipf * GameConstants.UserPowerMercy; }
+        set { _ipf = value; }
+    }
+
+    public float ExpiratoryPeakFlow
+    {
+        get { return _epf * GameConstants.UserPowerMercy; }
+        set { _epf = value; }
+    }
+
+    public float InspiratoryFlowTime
+    {
+        get { return _ift * GameConstants.UserPowerMercy; }
+        set { _ift = value; }
+    }
+
+    public float ExpiratoryFlowTime
+    {
+        get { return _eft * GameConstants.UserPowerMercy; }
+        set { _eft = value; }
+    }
+
+    public float RespirationFrequency
+    {
+        get { return _rf * GameConstants.UserPowerMercy; }
+        set { _rf = value; }
+    }
+
     public byte LastLevel { get; set; }
+
     public byte OpenLevel { get; set; }
+
     public uint TotalScore { get; set; }
+
     public uint SessionsDone { get; set; }
+
     public bool TutorialDone { get; set; }
 
     public Player()
