66053ea8db5ea427c215bb97df4fde7e5769dab6
Renato Grimes <rengrimes@gmail.com>
2017-09-27 05:14:28 -0300

    PositionOnSerial baseado em eventos

--- Assets/Scripts/Plataform/Player/PositionOnSerial.cs
+++ Assets/Scripts/Plataform/Player/PositionOnSerial.cs
@@ -1,132 +1,108 @@
-﻿using System.Collections;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PositionOnSerial : MonoBehaviour
 {
-	private Player _player;
-	private Transform _transform;
-	private SerialListener _serialMessager;
-	private bool _isUsingOffset;
-	private float _offset;
-	private float _cameraOffset;
+    private Player _player;
+    private Transform _transform;
+    private SerialListener _serialListener;
+    private float _cameraOffset;
 
-	private const float RelativeLimit = 0.3f;
+    private const float RelativeLimit = 0.3f;
 
-	public ControlBehaviour Behaviour;
+    public ControlBehaviour Behaviour;
 
-	private void Start()
-	{
-		_transform = GetComponent<Transform>();
-		_serialMessager = GetComponent<SerialListener>();
-		_isUsingOffset = false;
-		_cameraOffset = Camera.main.orthographicSize - Camera.main.transform.position.y - 1;
-		_player = GameManager.Instance?.Player;
+    private void Awake()
+    {
+        _transform = GetComponent<Transform>();
+        _serialListener = GetComponent<SerialListener>();
+        _cameraOffset = Camera.main.orthographicSize - Camera.main.transform.position.y - 1;
+        _player = GameManager.Instance?.Player;
+    }
 
-		StartCoroutine(GetOffset());
-	}
+    private void Update()
+    {
+        ChangeBehaviourHotkey();
+    }
 
-	private IEnumerator GetOffset()
-	{
-		while (!_serialMessager.IsConnected)
-			yield return new WaitForSeconds(3f);
+    private void ChangeBehaviourHotkey()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            switch (Behaviour)
+            {
+                case ControlBehaviour.Absolute:
+                    Behaviour = ControlBehaviour.Relative;
+                    break;
+                case ControlBehaviour.Relative:
+                    Behaviour = ControlBehaviour.Absolute;
+                    break;
+            }
 
-		var temp = 0f;
-		for (var i = 0; i < 5000; i++)
-		{
-			var message = _serialMessager.MessageReceived;
-			temp += string.IsNullOrEmpty(message) ? 0f : ParseSerialMessage(message);
-		}
+            Debug.LogFormat("Behaviour: {0}", Behaviour);
+        }
+    }
 
-		_offset = temp / 5000f;
-		Debug.Log($"Offset set to {_offset}");
+    private void OnEnable()
+    {
+        _serialListener.OnSerialMessageReceived += OnSerialMessageReceived;
+        PitacoRecorder.Instance.Start();
+    }
 
-		_isUsingOffset = true;
-	}
-
-	private void Update()
-	{
-		ChangeBehaviourHotkey();
-		PositionOnSerialMessage();
-	}
-
-	private void PositionOnSerialMessage()
-	{
-		if (!_isUsingOffset) return;
-
-		var message = _serialMessager.MessageReceived;
-		if (message.Length < 1) return;
-
-		var sensorValue = ParseSerialMessage(message) - _offset;
+    private void OnDisable()
+    {
+        _serialListener.OnSerialMessageReceived -= OnSerialMessageReceived;
+        PitacoRecorder.Instance.Stop();
 
 #if UNITY_EDITOR
-		var ExpiratoryPeakFlow = 300f * GameConstants.UserPowerMercy; //debug
-		var nextPosition = _cameraOffset * sensorValue / ExpiratoryPeakFlow;
-#else
-        Debug.Log($"UserPowerMercy: {GameConstants.UserPowerMercy}");
-        var nextPosition = (_cameraOffset * sensorValue / _player.ExpiratoryPeakFlow) * GameConstants.UserPowerMercy;
-#endif
-
-		Vector3 a = _transform.position;
-		Vector3 b = Vector3.zero;
-		switch (Behaviour)
-		{
-			case ControlBehaviour.Absolute:
-				b = new Vector3(_transform.position.x, nextPosition, _transform.position.z);
-				break;
-
-			case ControlBehaviour.Relative:
-				if (!(nextPosition >= -RelativeLimit && nextPosition <= RelativeLimit))
-				{
-					b = _transform.position + new Vector3(0f, nextPosition);
-				}
-				break;
-		}
-
-		_transform.position = Vector3.Lerp(a, b, Time.deltaTime * 5f);
-
-		PitacoRecorder.Instance.Add(_transform.position.y);
-	}
-
-	private static float ParseSerialMessage(string msg) => float.Parse(msg.Replace('.', ','));
-
-	private void OnCollisionEnter(Collision collision)
-	{
-		Destroy(collision.gameObject);
-	}
-
-	private void OnEnable()
-	{
-		PitacoRecorder.Instance.Start();
-	}
-
-	private void OnDisable()
-	{
-		PitacoRecorder.Instance.Stop();
-
-#if UNITY_EDITOR
-		PitacoRecorder.Instance.WriteData();
+        PitacoRecorder.Instance.WriteData();
 #else
         var stage = GameManager.Instance.Stage;
         PitacoRecorder.Instance.WriteData(_player, stage, GameConstants.GetSessionsPath(_player), true);
 #endif
 
-	}
+    }
 
-	private void ChangeBehaviourHotkey()
-	{
-		if (Input.GetKeyDown(KeyCode.Space))
-		{
-			switch (Behaviour)
-			{
-				case ControlBehaviour.Absolute:
-					Behaviour = ControlBehaviour.Relative;
-					break;
-				case ControlBehaviour.Relative:
-					Behaviour = ControlBehaviour.Absolute;
-					break;
-			}
+    private void OnSerialMessageReceived(string msg)
+    {
+        if (!SerialGetOffset.IsUsingOffset) return;
+        
+        if (msg.Length < 1) return;
 
-			Debug.LogFormat("Behaviour: {0}", Behaviour);
-		}
-	}
+        var sensorValue = GameConstants.ParseSerialMessage(msg) - SerialGetOffset.Offset;
+
+        sensorValue = (sensorValue < -1f || sensorValue > 1f) ? sensorValue : 0f; //ToDO threshold
+
+#if UNITY_EDITOR
+        var expiratoryPeakFlow = 300f; //debug
+        var nextPosition = _cameraOffset * sensorValue / expiratoryPeakFlow * GameConstants.UserPowerMercy;
+#else
+        Debug.Log($"UserPowerMercy: {GameConstants.UserPowerMercy}");
+        var nextPosition = _cameraOffset * sensorValue / _player.ExpiratoryPeakFlow * GameConstants.UserPowerMercy;
+#endif
+
+        Vector3 a = _transform.position;
+        Vector3 b = Vector3.zero;
+        switch (Behaviour)
+        {
+            case ControlBehaviour.Absolute:
+                b = new Vector3(_transform.position.x, nextPosition, _transform.position.z);
+                break;
+
+            case ControlBehaviour.Relative:
+                if (!(nextPosition >= -RelativeLimit && nextPosition <= RelativeLimit))
+                {
+                    b = _transform.position + new Vector3(0f, nextPosition);
+                }
+                break;
+        }
+
+        _transform.position = Vector3.Lerp(a, b, Time.deltaTime * 15f);
+
+        PitacoRecorder.Instance.Add(_transform.position.y);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(collision.gameObject);
+    }
 }
