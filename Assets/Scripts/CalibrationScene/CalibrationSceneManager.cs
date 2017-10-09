39bc297a8fd09686b978dd7ca3e2e08e35bc9aff
Renato Grimes <rengrimes@gmail.com>
2017-10-09 14:54:06 -0300

    Implementação do Expiration Time

--- Assets/Scripts/CalibrationScene/CalibrationSceneManager.cs
+++ Assets/Scripts/CalibrationScene/CalibrationSceneManager.cs
@@ -1,17 +1,21 @@
 ﻿using System.Collections;
+using System.Diagnostics;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using Debug = UnityEngine.Debug;
 
 public class CalibrationSceneManager : MonoBehaviour
 {
-    private bool _executeStep, _sceneOpen;
+    private bool _executeStep, _sceneOpen, _firstTimePlaying;
     private int _stepNum = 12; //ToDo - change back to 1 when code is done
+
+    private RespiratoryInfo _respiratoryInfoTemp;
 
     #region Flow Exercice Variables
 
     private float _flowMeter;
     private int _exerciseCounter;
+    private Stopwatch _stopwatch;
 
     #endregion
 
@@ -30,15 +34,24 @@ public class CalibrationSceneManager : MonoBehaviour
             GameManager.Instance.Player = new Player
             {
                 Name = "NetRunner",
-                Id = 0
+                Id = 0,
+                CalibrationDone = false,
             };
         }
 #endif
-        //ToDo - Translate const strings
+
+        // Initialization
+        _respiratoryInfoTemp = new RespiratoryInfo();
+        _firstTimePlaying = GameManager.Instance.Player.CalibrationDone;
+        GameManager.Instance.Player.CalibrationDone = false;
+        _stopwatch = new Stopwatch();
+
+        // ToDo - Translate const strings
         var firstTimeMsg = "Olá! Bem-vindo ao I Blue It!";
         var hereAgainMsg = "Olá! Vamos calibrar o PITACO novamente?";
-        firstTimeText.text = GameManager.Instance.Player.CalibrationDone ? hereAgainMsg : firstTimeMsg;
-        _stepNum = GameManager.Instance.Player.CalibrationDone ? 4 : _stepNum;
+        firstTimeText.text = _firstTimePlaying ? firstTimeMsg : hereAgainMsg;
+        _stepNum = _firstTimePlaying ? _stepNum : 4;
+
         enterButton.SetActive(true);
         serialController.OnSerialMessageReceived += OnSerialMessageReceived;
         StartCoroutine(ScreenSteps());
@@ -59,7 +72,6 @@ public class CalibrationSceneManager : MonoBehaviour
 
     private IEnumerator ScreenSteps()
     {
-        GameManager.Instance.Player.CalibrationDone = false;
         while (!GameManager.Instance.Player.CalibrationDone)
         {
             if (_executeStep)
@@ -79,7 +91,7 @@ public class CalibrationSceneManager : MonoBehaviour
                     // Fade black screen and wait 1s 
                     firstTimePanel.GetComponent<Image>().CrossFadeAlpha(0, 1, false);
                     yield return new WaitForSeconds(1);
-                    Destroy(firstTimePanel);
+                    firstTimePanel.SetActive(false);
                     Destroy(enterButton);
 
                     // Enable dude and clock
@@ -114,13 +126,13 @@ public class CalibrationSceneManager : MonoBehaviour
                     #region Expiration Peak
 
                     case 3:
-                        dudeMsg = "Este é o relógio que vai medir a força e o tempo da sua respiração.";
+                        dudeMsg = "Este é o relógio que vai medir a força e o tempo da sua respiração. Começando com a força.";
                         DudeShowMessage(dudeMsg);
                         SetNextStep();
                         break;
 
                     case 4: // Tell player to do a Expiratory Peak Exercise
-                        dudeMsg = "Quando o relógio ficar verde, inspire e assopre bem forte no PITACO. Faremos exercícios!";
+                        dudeMsg = "Quando o relógio ficar verde, inspire e assopre bem forte no PITACO. Faremos 3 exercícios!";
                         DudeShowMessage(dudeMsg);
                         SetNextStep();
                         break;
@@ -263,25 +275,119 @@ public class CalibrationSceneManager : MonoBehaviour
                     #region Expiration Time
 
                     case 12:
-                        throw new NotImplementedException();
+                        ResetExerciseCounter();
+                        ResetFlowMeter();
+                        dudeMsg = "Agora vamos medir o tempo de expiração. Relaxe e expire o máximo de tempo possível no PITACO.";
+                        DudeShowMessage(dudeMsg);
+                        SetNextStep();
                         break;
+
+                    case 13:
+                        if (!serialController.IsConnected)
+                        {
+                            WarnPitacoDisconnected();
+                            continue;
+                        }
+
+                        // Enable clock arrow spin
+                        clockArrowSpin.SpinClock = true;
+
+#if UNITY_EDITOR
+                        serialController.InitializePitacoRequest();
+                        while (!SerialGetOffset.IsUsingOffset) yield return null;
+#endif
+
+                        // Change clock color to green so player can use pitaco
+                        tutoClock.GetComponent<SpriteRenderer>().color = Color.green;
+                        balloonText.text = "Relaxe, expire o máximo de tempo possível e aguarde.";
+
+                        // Wait for player input to be greather than threshold
+                        _stopwatch.Reset();
+                        while (_flowMeter <= GameConstants.CalibrationThreshold)
+                            yield return null;
+
+
+                        _stopwatch.Start();
+
+                        while (_flowMeter > GameConstants.CalibrationThreshold)
+                            yield return null;
+
+                        _stopwatch.Stop();
+
+                        Debug.Log("X Seconds ");
+
+                        // Disable clock arrow spin and reset clock color
+                        tutoClock.GetComponent<SpriteRenderer>().color = Color.white;
+                        clockArrowSpin.SpinClock = false;
+
+                        // Check for player input
+                        var expTCheck = _flowMeter;
+                        ResetFlowMeter();
+
+                        if (expTCheck < -GameConstants.CalibrationThreshold)
+                        {
+                            _exerciseCounter++;
+
+                            if (_exerciseCounter == 2)
+                                SetStep(15, true);
+                            else
+                                SetNextStep(true);
+
+                            continue;
+                        }
+                        else
+                        {
+                            dudeMsg = "Não consegui medir sua expiração. Vamos tentar novamente?";
+                            DudeShowMessage(dudeMsg);
+                            SetStep(13);
+                            break;
+                        }
+
+                    case 14:
+                        dudeMsg = "Muito bem!";
+                        DudeShowMessage(dudeMsg);
+                        //todo - quicky claps sounds
+                        SetStep(_exerciseCounter == 3 ? 16 : 15);
+                        break;
+
+                    case 15:
+                        dudeMsg = "Mais uma vez!";
+                        DudeShowMessage(dudeMsg);
+                        SetStep(9);
+                        break;
+
 
                     #endregion
 
                     #region Inspiration Time
+
+                    case 16:
+                        break;
+
                     #endregion
 
                     #region Flow Measurement
                     #endregion
 
+                    case 998:
+                        dudeMsg = "Ótimo, você está pronto para começar a jogar! Bom jogo!";
+                        var dudeMsgAgain = "Ótimo, agora você está pronto para voltar a jogar! Até mais!";
+                        DudeShowMessage(_firstTimePlaying ? dudeMsg : dudeMsgAgain);
+                        SetStep(9);
+                        break;
+
                     case 999: //ToDo - change this later
-                        levelLoader.LoadScene(2);
                         GameManager.Instance.Player.CalibrationDone = true;
+                        GameManager.Instance.Player.RespiratoryInfo = _respiratoryInfoTemp;
+                        DatabaseManager.Instance.Players.Save();
+                        firstTimePanel.SetActive(false);
+                        firstTimePanel.GetComponent<Image>().CrossFadeAlpha(1, 1, false);
+                        yield return new WaitForSeconds(1.5f);
+                        levelLoader.LoadScene(2);
                         break;
 
                     default:
-                        var activeScene = SceneManager.GetActiveScene().buildIndex;
-                        levelLoader.LoadScene(activeScene);
+                        levelLoader.LoadScene(3); // Calibration Scene Build Index
                         break;
                 }
 
@@ -342,10 +448,10 @@ public class CalibrationSceneManager : MonoBehaviour
                     {
                         _flowMeter = tmp;
 
-                        if (_flowMeter > GameManager.Instance.Player.RespiratoryInfo.ExpiratoryPeakFlow)
+                        if (_flowMeter > _respiratoryInfoTemp.ExpiratoryPeakFlow)
                         {
-                            GameManager.Instance.Player.RespiratoryInfo.ExpiratoryPeakFlow = _flowMeter;
-                            Debug.Log($"ExpiratoryPeakFlow: {GameManager.Instance.Player.RespiratoryInfo.ExpiratoryPeakFlow}");
+                            _respiratoryInfoTemp.ExpiratoryPeakFlow = _flowMeter;
+                            Debug.Log($"ExpiratoryPeakFlow: {_respiratoryInfoTemp.ExpiratoryPeakFlow}");
                         }
                     }
                     break;
@@ -355,10 +461,10 @@ public class CalibrationSceneManager : MonoBehaviour
                     {
                         _flowMeter = tmp;
 
-                        if (_flowMeter < GameManager.Instance.Player.RespiratoryInfo.InspiratoryPeakFlow)
+                        if (_flowMeter < _respiratoryInfoTemp.InspiratoryPeakFlow)
                         {
-                            GameManager.Instance.Player.RespiratoryInfo.InspiratoryPeakFlow = _flowMeter;
-                            Debug.Log($"InspiratoryPeakFlow: {GameManager.Instance.Player.RespiratoryInfo.InspiratoryPeakFlow}");
+                            _respiratoryInfoTemp.InspiratoryPeakFlow = _flowMeter;
+                            Debug.Log($"InspiratoryPeakFlow: {_respiratoryInfoTemp.InspiratoryPeakFlow}");
                         }
                     }
                     break;
