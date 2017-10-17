7ebcdf54ef3442e247f88b96c024e2ae775d9687
Renato Grimes <rengrimes@gmail.com>
2017-10-17 18:52:10 -0200

    Removido overload de WriteData

--- Assets/Scripts/PITACO/PitacoRecorder.cs
+++ Assets/Scripts/PITACO/PitacoRecorder.cs
@@ -37,51 +37,37 @@ public class PitacoRecorder
         _incomingDataDictionary.Add(_stopwatch.ElapsedMilliseconds, value);
     }
 
-    public void WriteData(string path = null, bool clearRecords = false)
+    public void WriteData(Player plr, Stage stg, bool clearRecords = false)
     {
-        if (_incomingDataDictionary.Count == 0) return;
+        if (_incomingDataDictionary.Count == 0)
+            return;
 
         UnityEngine.Debug.Log($"Writing {_incomingDataDictionary.Count} values from incoming data dictionary...");
+
+        var configString = new[]
+        {
+            "SessionId", "PlayerId", "PlayerName", "PlayerDisfunction", "SessionStart", "SessionFinish", "StageId"
+        };
+        _sb.AppendLine(configString.Aggregate((a, b) => a + ";" + b));
+
+        _sb.AppendLine($"{plr.SessionsDone};{plr.Id};{plr.Name};{plr.Disfunction};{_recordStart};{_recordFinish};{stg.Id};");
+        _sb.AppendLine();
 
         foreach (var pair in _incomingDataDictionary)
         {
             _sb.AppendLine($"{pair.Key};{pair.Value}");
         }
 
-        string filepath;
+        string filePath;
 
-        if (string.IsNullOrEmpty(path))
-        {
-            filepath = GameConstants.SaveDataPath + $"test_{_recordStart:yyyyMMdd_hhmmss}.csv";
-        }
-        else
-        {
-            filepath = path + _recordStart.ToString("yyyyMMdd_hhmmss") + ".csv";
-        }
+        filePath = plr != null ? GameConstants.GetSessionsPath(plr) + $"PITACO-SESSION_{plr.SessionsDone}.csv" : GameConstants.SaveDataPath + $"PITACO-SESSION_{_recordStart:yyyyMMdd_HHmmss}.csv";
 
-        GameUtilities.WriteAllText(filepath, _sb.ToString());
+        GameUtilities.WriteAllText(filePath, _sb.ToString());
 
         if (clearRecords)
         {
             ClearRecords();
         }
-    }
-
-    public void WriteData(Player plr, Stage stg, bool clearRecords = false)
-    {
-        var configString = new[]
-        {
-            "PlayerID", "PlayerName", "PlayerDisfunction", "SessionStart", "SessionFinish", "StageId"
-        };
-
-        _sb.AppendLine(configString.Aggregate((a, b) => a + ";" + b));
-
-        _sb.AppendLine($"{plr.Id};{plr.Name};{plr.Disfunction};{_recordStart};{_recordFinish};{stg.Id};");
-        _sb.AppendLine();
-
-        var path = GameConstants.GetSessionsPath(plr);
-
-        WriteData(path, clearRecords);
     }
 
     public void ClearRecords()
