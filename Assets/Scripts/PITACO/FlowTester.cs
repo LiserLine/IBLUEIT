517b7e904814f74f20b3bdbfcf808bc1554fd6de
Renato Grimes <rengrimes@gmail.com>
2017-10-19 14:58:26 -0200

    Adicionado cena de teste de fluxo

---
+++ Assets/Scripts/PITACO/FlowTester.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class FlowTester : MonoBehaviour
+{
+    public SerialController SerialController;
+
+    private void Start()
+    {
+        SerialController.OnSerialMessageReceived += OnSerialMessageReceived;
+    }
+
+    private void OnEnable()
+    {
+        PitacoRecorder.Instance.StartRecording();
+    }
+
+    private void OnDisable()
+    {
+        PitacoRecorder.Instance.StopRecording();
+        PitacoRecorder.Instance.WriteData(null, null, true);
+    }
+
+    private void OnSerialMessageReceived(string msg)
+    {
+        if (!SerialGetOffset.IsUsingOffset) return;
+
+        if (msg.Length < 1) return;
+
+        var sensorValue = GameUtilities.ParseSerialMessage(msg) - SerialGetOffset.Offset;
+
+        PitacoRecorder.Instance.RecordValue(sensorValue);
+    }
+}
