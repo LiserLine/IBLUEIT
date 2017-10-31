1d035712b27afdcb6c3f8346881ba5113369d03b
Renato Grimes <rengrimes@gmail.com>
2017-10-31 18:52:21 -0200

    Montagem do sineOnTime

--- Assets/Scripts/PlataformScene/Enemies/Spawner.cs
+++ Assets/Scripts/PlataformScene/Enemies/Spawner.cs
@@ -2,22 +2,19 @@
 
 public class Spawner : MonoBehaviour
 {
-    private Transform _transform;
-    private float _delta;
+    private float _dt;
     private float _spawnEveryXSec;
-    private float _movementSpeed;
+    private float _plrBasedHeight;
 
     private float _levelInfluence = 1; // ToDo - based on CSV
-    private float _disfuncionInfluencer = 1; // ToDo - based on Enum
+    private float _disfunctionInfluence = 1; // ToDo - based on Enum
 
-    [Header("Settings")]
-    public GameObject[] Obstacles = new GameObject[1];
-    public GameObject[] Targets = new GameObject[1];
+    public GameObject[] Obstacles;
+    public GameObject[] Targets;
 
     private void OnEnable()
     {
-        _transform = GetComponent<Transform>();
-        _movementSpeed = GameManager.Instance.Player.RespiratoryInfo.RespirationFrequency * _levelInfluence;
+        _plrBasedHeight = GameManager.Instance.Player.RespiratoryInfo.RespirationFrequency * _levelInfluence;
         _spawnEveryXSec = GameManager.Instance.Player.RespiratoryInfo.RespirationFrequency;
     }
 
@@ -26,36 +23,44 @@ public class Spawner : MonoBehaviour
         if (!SerialGetOffset.IsUsingOffset)
             return;
 
-        SetYPosision();
-        ReleaseObject();
+        UpdatePosition();
+
+        _dt += Time.deltaTime;
+        if (_dt >= _spawnEveryXSec)
+        {
+            ReleaseObject();
+            _dt = 0f;
+        }
     }
 
-    private void SetYPosision()
+    private void UpdatePosition()
     {
-        _transform.position = new Vector3(_transform.position.x,
-            Mathf.Sin(Time.deltaTime * _movementSpeed) * GameManager.Instance.Player.RespiratoryInfo.ExpiratoryPeakFlow,
-            _transform.position.z);
+        var sineOnTime = Mathf.Sin(Time.time);
+        var cameraHeightFromZero = 9; // ToDo - get this properlly
+
+        var nextPos = new Vector3
+        {
+            x = this.transform.position.x,
+            y = sineOnTime * cameraHeightFromZero / (sineOnTime >= 0f ? GameManager.Instance.Player.RespiratoryInfo.ExpiratoryPeakFlow : GameManager.Instance.Player.RespiratoryInfo.InspiratoryPeakFlow) ,
+            z = this.transform.position.z
+        };
+
+        this.transform.position = nextPos;
     }
 
     private void ReleaseObject()
     {
-        _delta += Time.deltaTime;
-
-        if (_delta < _spawnEveryXSec) return;
-
         var rnd = Random.Range(0, 2);
         if (rnd == 0) //Obstacles
         {
-            //Instantiate(Obstacles[0], _transform); <- Waveform movement
+            //Instantiate(Obstacles[0], this.transform); <- Waveform movement
             var size = Random.Range(1, 4);
             Obstacles[0].transform.localScale = new Vector3(size, size * 3, 1f);
-            Instantiate(Obstacles[0], new Vector3(_transform.position.x, 0f), _transform.rotation);
+            Instantiate(Obstacles[0], new Vector3(this.transform.position.x, 0f), this.transform.rotation);
         }
         else //Items
         {
-            Instantiate(Targets[0], _transform.position, _transform.rotation);
+            Instantiate(Targets[0], this.transform.position, this.transform.rotation);
         }
-
-        _delta = 0f;
     }
 }
