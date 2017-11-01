e1f55df6021d202f9e9cf67c223908bd1b5fafb7
Renato Grimes <rengrimes@gmail.com>
2017-11-01 11:51:21 -0200

    Melhor controle do SceneManager

--- Assets/Scripts/PlataformScene/PlataformSceneManager.cs
+++ Assets/Scripts/PlataformScene/PlataformSceneManager.cs
@@ -1,15 +1,38 @@
 ﻿using UnityEngine;
 
 public class PlataformSceneManager : MonoBehaviour
-{    
-    void Start()
-    {
+{
+    private float _dt;
 
+    private void Update()
+    {
+        if (GameManager.Instance.Stage != null && GameManager.Instance.Stage.IsRunning && SerialGetOffset.IsUsingOffset)
+        {
+            _dt += Time.deltaTime;
+            if (_dt >= ((PlataformStage)GameManager.Instance.Stage).TimeLimit)
+            {
+                EndStage();
+            }
+        }
     }
-    
-    void Update()
+
+    public void StartStage(int stageId)
     {
+        GameManager.Instance.Stage = new PlataformStage { Id = stageId };
+        GameManager.Instance.Stage.Start();
+    }
 
+    public void EndStage()
+    {
+        if (!GameManager.Instance.Stage.IsRunning)
+            return;
 
-    }    
+        var sComm = GameObject.FindGameObjectWithTag("SerialController");
+        Destroy(sComm);
+
+        Debug.LogWarning("ToDo - write player session data"); // ToDo - write player session data
+
+        GameManager.Instance.Stage.Stop();
+        Debug.Log($"Stage {GameManager.Instance.Stage.Id} terminated.");
+    }
 }
