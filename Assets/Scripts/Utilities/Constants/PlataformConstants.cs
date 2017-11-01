d6e796818c91bf9b771639b4eb5c147bd437b6ef
Renato Grimes <rengrimes@gmail.com>
2017-11-01 11:51:54 -0200

    Adicionado Constantes da Plataforma

---
+++ Assets/Scripts/Utilities/Constants/PlataformConstants.cs
@@ -0,0 +1,37 @@
+﻿// ToDo - Load from CSV
+
+public class PlataformConstants
+{
+    public TargetHeightMultiplier TargetHeightMultiplier = new TargetHeightMultiplier();
+    public ObstacleSizeMultiplier ObstacleSizeMultiplier = new ObstacleSizeMultiplier();
+    public SpawnQuantity SpawnQuantity = new SpawnQuantity();
+}
+
+public class TargetHeightMultiplier
+{
+    public readonly float Min = 0.33f;
+    public readonly float Med = 0.66f;
+    public readonly float Max = 1f;
+}
+
+public class ObstacleSizeMultiplier
+{
+    public readonly float Min = 0.33f;
+    public readonly float Med = 0.66f;
+    public readonly float Max = 1f;
+}
+
+public class SpawnQuantity
+{
+    public readonly int Stage1 = 7;
+    public readonly int Stage2 = 8;
+    public readonly int Stage3 = 10;
+
+    public readonly int Stage4 = 11;
+    public readonly int Stage5 = 12;
+    public readonly int Stage6 = 15;
+
+    public readonly int Stage7 = 15;
+    public readonly int Stage8 = 17;
+    public readonly int Stage9 = 20;
+}
