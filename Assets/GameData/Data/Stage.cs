6107b7a0ea616240bb8dc84a529d2ec3cd2516cd
Renato Grimes <rengrimes@gmail.com>
2017-11-01 11:50:18 -0200

    Adicionado novas variáveis do Stage

--- Assets/GameData/Data/Stage.cs
+++ Assets/GameData/Data/Stage.cs
@@ -1,5 +1,85 @@
-﻿public class Stage
+﻿using System;
+
+public class Stage
 {
+    public delegate void StageStartHandler();
+    public event StageStartHandler OnStageStart;
+
+    public delegate void StageEndHandler();
+    public event StageEndHandler OnStageEnd;
+
     public int Id { get; set; }
+    public bool IsRunning { get; set; }
+
+    public virtual void Start()
+    {
+        OnStageStart?.Invoke();
+        IsRunning = true;
+    }
+
+    public virtual void Stop()
+    {
+        OnStageEnd?.Invoke();
+        IsRunning = false;
+    }
+}
+
+public class PlataformStage : Stage
+{
+    /// <summary>
+    /// Stage time limit
+    /// </summary>
+    public int TimeLimit { get; private set; }
+
+    /// <summary>
+    /// Objects to spawn during the game
+    /// </summary>
+    public int SpawnQuantitity { get; private set; }
+
+    /// <summary>
+    /// Spawn delay for Spawner (SpawnQuantitity / TimeLimit)
+    /// </summary>
+    public int SpawnDelay
+    {
+        get
+        {
+            if (!IsRunning)
+                throw new Exception("Plataform is not running. Start a stage to use SpawnDelay.");
+
+            return SpawnQuantitity / TimeLimit;
+        }
+    }
+
+    public PlataformElements Elements { get; private set; }
+    public float ObstacleSizeMultiplier { get; private set; }
+    public float TargetHeightMultiplier { get; private set; }
+
+    public override void Start()
+    {
+        if (Id >= 1 && Id <= 3)
+        {
+            Elements = PlataformElements.Targets;
+            TargetHeightMultiplier = Id == 3 ? GameConstants.Plataform.TargetHeightMultiplier.Med : GameConstants.Plataform.TargetHeightMultiplier.Min;
+            SpawnQuantitity = Id == 1 ? GameConstants.Plataform.SpawnQuantity.Stage1 : (Id == 2 ? GameConstants.Plataform.SpawnQuantity.Stage2 : GameConstants.Plataform.SpawnQuantity.Stage3);
+            TimeLimit = 60;
+        }
+        else if (Id >= 4 && Id <= 6)
+        {
+            Elements = PlataformElements.TargetAndObstacles;
+            TargetHeightMultiplier = Id == 4 ? GameConstants.Plataform.TargetHeightMultiplier.Med : GameConstants.Plataform.TargetHeightMultiplier.Max;
+            ObstacleSizeMultiplier = Id == 6 ? GameConstants.Plataform.ObstacleSizeMultiplier.Med : GameConstants.Plataform.ObstacleSizeMultiplier.Min;
+            SpawnQuantitity = Id == 4 ? GameConstants.Plataform.SpawnQuantity.Stage4 : (Id == 5 ? GameConstants.Plataform.SpawnQuantity.Stage5 : GameConstants.Plataform.SpawnQuantity.Stage6);
+            TimeLimit = 90;
+        }
+        else if (Id >= 7 && Id <= 9)
+        {
+            Elements = PlataformElements.Obstacles;
+            ObstacleSizeMultiplier = Id == 7 ? GameConstants.Plataform.ObstacleSizeMultiplier.Med : GameConstants.Plataform.ObstacleSizeMultiplier.Max;
+            SpawnQuantitity = Id == 7 ? GameConstants.Plataform.SpawnQuantity.Stage7 : (Id == 8 ? GameConstants.Plataform.SpawnQuantity.Stage8 : GameConstants.Plataform.SpawnQuantity.Stage9);
+            TimeLimit = 120;
+        }
+
+        base.Start();
+    }
 }
 
