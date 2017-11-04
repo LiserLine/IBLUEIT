89e8d1f0e3137bc0e6437ca15b21d4322194fb37
Renato Grimes <rengrimes@gmail.com>
2017-11-03 22:09:46 -0200

    Uso do tempo da respiração no spawner
    Tinha esquecido :P

--- Assets/Scripts/PlataformScene/Enemies/Spawner.cs
+++ Assets/Scripts/PlataformScene/Enemies/Spawner.cs
@@ -16,7 +16,7 @@ public class Spawner : MonoBehaviour
         var stage = (PlataformStage)GameManager.Instance.Stage;
         _spawnEveryXSec = (float)stage.TimeLimit / stage.SpawnQuantitity;
         stage.OnStageEnd += DestroySpawnedObjects;
-        _distanceBetweenSpawns = GameManager.Instance.Player.Disfunction == Disfunctions.Restrictive ? 7f : 14f;
+        _distanceBetweenSpawns = 10f; // ToDO - is this good enough?
         _dt = _spawnEveryXSec;
         _cameraBounds = 9; // ToDo - get this properlly
     }
@@ -95,24 +95,37 @@ public class Spawner : MonoBehaviour
         var obstacleIndex1 = Random.Range(0, Obstacles.Length); // TODO properlly - alguns itens podem ser submersos, outros n
         var obstacleIndex2 = Random.Range(0, Obstacles.Length); // TODO properlly
 
+        //ToDO - Remove if not used anymore
+        //var disfunctionMultiplier = (float)GameManager.Instance.Player.Disfunction;
+
+        //random //ToDO - Remove if not used anymore
         //var randomHeightOffset = Random.Range(0, 2) * 2 - 1; //ToDo - this is strange during gameplay
-
-        var disfunctionMultiplier = (float)GameManager.Instance.Player.Disfunction;
-
         //var go = Instantiate(Obstacles[obstacleIndex1], new Vector3(this.transform.position.x, -randomHeightOffset), this.transform.rotation);
         //var go2 = Instantiate(Obstacles[obstacleIndex2], new Vector3(this.transform.position.x + _distanceBetweenSpawns, randomHeightOffset), this.transform.rotation);
 
         var go = Instantiate(Obstacles[obstacleIndex1], new Vector3(this.transform.position.x, 0f), this.transform.rotation);
         var go2 = Instantiate(Obstacles[obstacleIndex2], new Vector3(this.transform.position.x + _distanceBetweenSpawns, 0f), this.transform.rotation);
 
-        go.transform.localScale = new Vector3(go.transform.localScale.x * sizeMultiplier, go.transform.localScale.y * sizeMultiplier, 1);
-        go2.transform.localScale = new Vector3(go2.transform.localScale.x * sizeMultiplier * disfunctionMultiplier, go2.transform.localScale.y * sizeMultiplier * disfunctionMultiplier, 1);
+        //ToDO - Remove if not used anymore
+        //go.transform.localScale = new Vector3(go.transform.localScale.x * sizeMultiplier, go.transform.localScale.y * sizeMultiplier, 1);
+        //go2.transform.localScale = new Vector3(go2.transform.localScale.x * sizeMultiplier * disfunctionMultiplier, go2.transform.localScale.y * sizeMultiplier * disfunctionMultiplier, 1);
+
+        //ToDo - scales must be based on ins/exp on random too (if necessary)
+        var scaleIns = GameManager.Instance.Player.RespiratoryInfo.InspiratoryFlowTime / 1000;
+        var scaleExp = GameManager.Instance.Player.RespiratoryInfo.ExpiratoryFlowTime / 1000;
+
+        //ToDo - Do I really need this?
+        scaleExp *= GameConstants.UserPowerMercy + 1;
+        scaleIns *= GameConstants.UserPowerMercy + 1;
+
+        go.transform.localScale = new Vector3(scaleIns, scaleIns, 1);
+        go2.transform.localScale = new Vector3(scaleExp, scaleExp, 1);
 
         var goPos = go.transform.position;
         var go2Pos = go2.transform.position;
 
-        goPos.y += go.transform.localScale.y / 2;
-        go2Pos.y -= go2.transform.localScale.y / 2;
+        goPos.y -= go.transform.localScale.y / 2;
+        go2Pos.y += go2.transform.localScale.y / 2;
 
         go.transform.position = goPos;
         go2.transform.position = go2Pos;
