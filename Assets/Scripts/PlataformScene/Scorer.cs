488511c4111212505173111b865a96c7da5f64e4
Renato Grimes <rengrimes@gmail.com>
2017-11-06 01:50:26 -0200

    Scorer adicionado

---
+++ Assets/Scripts/PlataformScene/Scorer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Scorer : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag.Contains("Obstacle"))
+        {
+            ((PlataformStage)GameManager.Instance.Stage).ObstacleAvoided();
+        }
+
+        Destroy(collision.gameObject);
+    }
+}
