2581d37fa44623f0efcb5936ac07ce493a67896b
Renato Grimes <huenato@users.noreply.github.com>
2018-01-09 23:44:38 -0200

    Debugger Prefab

---
+++ Assets/_Game/Debugger/Scripts/DebuggingTools.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DebuggingTools : MonoBehaviour
+{
+    [SerializeField]
+    private string sceneToLoad;
+
+    public void Awake()
+    {
+#if UNITY_EDITOR
+#else
+        Destroy(this.gameObject);
+#endif
+    }
+
+    [ContextMenu("Load Selected Scene")]
+    private void LoadScene()
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+}
