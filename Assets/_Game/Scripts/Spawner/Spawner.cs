d4309e69a5530f4477cf02cdad0d8c23d7e33f62
Renato Grimes <huenato@users.noreply.github.com>
2018-01-13 03:45:19 -0200

    Cleaner movido

--- Assets/_Game/Scripts/Spawner/Spawner.cs
+++ Assets/_Game/Scripts/Spawner/Spawner.cs
@@ -167,6 +167,19 @@ public partial class Spawner : Singleton<Spawner>
         Clean();
     }
 
+    private void Clean()
+    {
+        //ToDo - clean anim? like cuphead or w/e
+
+        if (objectsOnScene.Count < 1)
+            return;
+
+        foreach (var go in objectsOnScene)
+            Destroy(go);
+
+        objectsOnScene.Clear();
+    }
+
     private void RemoveObject(GameObject goner)
     {
         objectsOnScene.Remove(goner);
