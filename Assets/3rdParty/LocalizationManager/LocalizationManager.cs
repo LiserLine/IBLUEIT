d267d9f623305315023879c3d47d9aa66dd24636
Renato Grimes <huenato@users.noreply.github.com>
2018-01-17 06:02:15 -0200

    Arquivos do LocalizationManager

---
+++ Assets/3rdParty/LocalizationManager/LocalizationManager.cs
@@ -0,0 +1,67 @@
+﻿// Source: https://www.youtube.com/watch?v=5Kt9jbnqzKA&list=PLX2vGYjWbI0TWkV9aEYq93bOX2kwseqUT
+
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class LocalizationManager : Singleton<LocalizationManager>
+{
+    private Dictionary<string, string> _localizedText;
+
+    public bool IsReady { get; private set; }
+    public bool IsLoaded { get; private set; }
+
+    private void Start()
+    {
+#if UNITY_EDITOR
+        //LoadLocalizationData("localizationData_brazilian.json");
+#endif
+    }
+
+    public void LoadLocalizationData(string filename)
+    {
+        if (IsLoaded)
+            return;
+
+        _localizedText = new Dictionary<string, string>();
+
+        var filepath = Application.streamingAssetsPath + Path.AltDirectorySeparatorChar + filename;
+
+#if UNITY_ANDROID
+        var www = new WWW(filepath);
+
+        if (!www.isDone)
+        {
+            Debug.Log("Waiting www...");
+        }
+
+        if (!string.IsNullOrEmpty(www.error))
+        {
+            Debug.LogErrorFormat("WWW Failed to load localized text on {0}", filepath);
+            return;
+        }
+
+        var jsonData = www.text;
+#else
+        if (!File.Exists(filepath))
+        {
+            Debug.LogErrorFormat("Failed to load localized text on {0}", filepath);
+            return;
+        }
+
+        var jsonData = Utils.ReadAllText(filepath);
+#endif
+
+        var loadedData = JsonUtility.FromJson<LocalizationData>(jsonData);
+
+        foreach (var localizationItem in loadedData.Items)
+            _localizedText.Add(localizationItem.Key, localizationItem.Value);
+
+        Debug.LogFormat("LocalizationManager loaded {0} items.", _localizedText.Count);
+
+        IsLoaded = true;
+        IsReady = true;
+    }
+
+    public string GetLocalizedValue(string key) => !_localizedText.ContainsKey(key) ? key : _localizedText[key];
+}
---
+++ Assets/3rdParty/LocalizationManager/LocalizedText.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class LocalizedText : MonoBehaviour
+{
+    public string Key;
+    private Text _text;
+
+    private void Start()
+    {
+        _text = this.GetComponent<Text>();
+        _text.text = LocalizationManager.Instance.GetLocalizedValue(Key);
+    }
+}
