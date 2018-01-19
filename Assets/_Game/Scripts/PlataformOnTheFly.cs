a54f193da007ef82d1e2bcc09aed22a0ff335b10
Renato Grimes <huenato@users.noreply.github.com>
2018-01-19 21:57:36 -0200

    Comandos on the fly na plataforma

---
+++ Assets/_Game/Scripts/PlataformOnTheFly.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class PlataformOnTheFly : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F2))
+            SerialController.Instance.Recalibrate();
+    }
+}
