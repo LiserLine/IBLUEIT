2557f22cdaf1b5e871dbf12431be78d8a04a5a7d
Renato Grimes <huenato@users.noreply.github.com>
2018-01-25 20:38:27 -0200

    Texto de Nível na HUD

---
+++ Assets/StageUI.cs
@@ -0,0 +1,16 @@
+﻿/**
+ * StageUI.cs
+ * Created by: Renato Grimes
+ * Created on: 25/01/2018 (dd/mm/yy)
+ */
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StageUI : BasicUI<StageUI>
+{
+    [SerializeField]
+    private Text stageIdText;
+
+    private void OnEnable() => stageIdText.text = Spawner.StageToLoad.ToString();
+}
