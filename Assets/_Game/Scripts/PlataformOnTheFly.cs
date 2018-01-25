63aebb688be296a88f0f83ae077dd4a7def2c1a0
Renato Grimes <huenato@users.noreply.github.com>
2018-01-25 13:57:44 -0200

    Pause nos botões Space e ESC

--- Assets/_Game/Scripts/PlataformOnTheFly.cs
+++ Assets/_Game/Scripts/PlataformOnTheFly.cs
@@ -6,5 +6,19 @@ public class PlataformOnTheFly : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.F2))
             SerialController.Instance.Recalibrate();
+
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.Space))
+        {
+            if (GameMaster.Instance.GameIsPaused)
+            {
+                FindObjectOfType<PauseMenuUI>().Hide();
+                GameMaster.Instance.UnPauseGame();
+            }
+            else
+            {
+                FindObjectOfType<PauseMenuUI>().Show();
+                GameMaster.Instance.PauseGame();
+            }
+        }
     }
 }
