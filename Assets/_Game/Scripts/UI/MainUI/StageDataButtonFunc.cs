1749370b2c68739825aeb0d424e2f2102be74d7c
Renato Grimes <huenato@users.noreply.github.com>
2018-01-31 01:20:02 -0200

    Mostra aviso antes de começar a jogar

--- Assets/_Game/Scripts/UI/MainUI/StageDataButtonFunc.cs
+++ Assets/_Game/Scripts/UI/MainUI/StageDataButtonFunc.cs
@@ -4,6 +4,12 @@ public class StageDataButtonFunc : MonoBehaviour
 {
     public void LoadStage()
     {
+        if (!SerialController.Instance.IsConnected)
+        {
+            SysMessage.Warning("PITACO não está conectado! Verifique a conexão!");
+            return;
+        }
+
         Spawner.StageToLoad = this.GetComponent<StageHolder>().StageToLoad;
         SceneLoader.Instance.LoadScene(1);
     }
