30445404eb275d50d9f8ef1773d6663c9ddefce1
Renato Grimes <huenato@users.noreply.github.com>
2018-02-05 12:00:55 -0200

    Botão de informação do jogador

--- Assets/_Game/Scripts/UI/MainUI/PlayerMenuUI.cs
+++ Assets/_Game/Scripts/UI/MainUI/PlayerMenuUI.cs
@@ -1,9 +1,14 @@
-﻿using System;
-
-public class PlayerMenuUI : BasicUI<PlayerMenuUI>
+﻿public class PlayerMenuUI : BasicUI<PlayerMenuUI>
 {
     public void ShowPlayerInfo()
     {
-        throw new NotImplementedException();
+        SysMessage.Info($"Jogador: {Pacient.Loaded.Name}\n" +
+                        $"Condição: {Pacient.Loaded.Disfunction}\n" +
+                        $"Partidas Jogadas: {Pacient.Loaded.PlaySessionsDone}\n" +
+                        $"Pico Exp.: {Pacient.Loaded.RespiratoryData.ExpiratoryPeakFlow}Pa\n" +
+                        $"Pico Ins.: {Pacient.Loaded.RespiratoryData.InspiratoryPeakFlow} Pa\n" +
+                        $"Tempo Exp.: {Pacient.Loaded.RespiratoryData.ExpiratoryFlowTime / 1000f:F1}s\n" +
+                        $"Tempo Ins.: {Pacient.Loaded.RespiratoryData.InspiratoryFlowTime / 1000f:F1}s\n" +
+                        $"Freq. Resp. Média: {Pacient.Loaded.RespiratoryData.RespirationFrequency / 1000f:F1}s");
     }
 }
