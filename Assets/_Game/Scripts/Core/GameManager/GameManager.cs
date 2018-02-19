213e3be861de9a98de9c68bb5a25285eaff17dbb
Renato Grimes <huenato@users.noreply.github.com>
2018-02-19 17:37:30 -0300

    Bug de perda de instancia do GameManager

--- Assets/_Game/Scripts/Core/GameManager/GameManager.cs
+++ Assets/_Game/Scripts/Core/GameManager/GameManager.cs
@@ -2,31 +2,20 @@
 
 public partial class GameManager : MonoBehaviour
 {
-    private static GameManager _instance;
     private static bool isLoaded;
+
     public static float CapacityMultiplier { get; private set; } = 0.5f;
     public static float LevelUnlockScoreThreshold { get; private set; } = 0.7f;
     public static float PitacoFlowThreshold { get; private set; } = 7.5f;
 
 #if UNITY_EDITOR
-
     public void QuitGame() => Debug.Log("Quit Game pressed!");
-
 #else
     public void QuitGame() => System.Diagnostics.Process.GetCurrentProcess().Kill();
 #endif
 
     private void Awake()
     {
-        if (_instance != null)
-        {
-            Destroy(this.gameObject);
-            return;
-        }
-
-        _instance = this;
-        DontDestroyOnLoad(this.gameObject);
-
         if (isLoaded)
             return;
 
