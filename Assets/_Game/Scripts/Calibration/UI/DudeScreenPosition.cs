5aeebf17b71f59b02843cc4b032694edc8676010
Renato Grimes <huenato@users.noreply.github.com>
2018-02-20 02:28:39 -0300

    Dude fora da tela em 4:3

---
+++ Assets/_Game/Scripts/Calibration/UI/DudeScreenPosition.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace _Game.Scripts.Calibration.UI
+{
+    public class DudeScreenPosition : MonoBehaviour
+    {
+        private void Awake()
+        {
+            var distance = Camera.main.orthographicSize * Camera.main.aspect;
+            this.gameObject.transform.Translate(-distance + (this.gameObject.transform.localScale.x / 2f), 0f, 0f);
+        }
+    }
+}
