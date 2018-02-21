dbbf5d8b51ea987092918199528ad441f8c295e0
Renato Grimes <huenato@users.noreply.github.com>
2018-02-21 02:19:09 -0300

    GameScoreRatio no HUD

---
+++ Assets/_Game/Scripts/Plataform/UI/GameScoreRatioUI.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameScoreRatioUI : MonoBehaviour
+{
+    [SerializeField]
+    private Text value;
+
+    private Scorer scorer;
+
+    private void Awake() => scorer = FindObjectOfType<Scorer>();
+
+    private void FixedUpdate()
+    {
+        value.text = $"{(scorer.Score / scorer.MaxScore * 100f):####}%";
+        value.color = scorer.Score >= scorer.MaxScore * GameManager.LevelUnlockScoreThreshold ? Color.blue : Color.red;
+    }
+}
