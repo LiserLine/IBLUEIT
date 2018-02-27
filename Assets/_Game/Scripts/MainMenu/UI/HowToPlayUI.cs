ff65e3f2827970d61207d68850e6fe0ef00ab577
Renato Grimes <huenato@users.noreply.github.com>
2018-02-27 16:49:13 -0300

    Painel Tutorial melhorado

--- Assets/_Game/Scripts/MainMenu/UI/HowToPlayUI.cs
+++ Assets/_Game/Scripts/MainMenu/UI/HowToPlayUI.cs
@@ -1,16 +1,59 @@
 ﻿using Ibit.Core.Data;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Ibit.MainMenu.UI
 {
     public class HowToPlayUI : MonoBehaviour
     {
+        [SerializeField]
+        private Image image;
+
+        [SerializeField]
+        private Sprite[] imageList;
+
+        private int iterator;
+
         private void OnEnable()
         {
             if (Pacient.Loaded.HowToPlayDone)
-                this.gameObject.SetActive(false);
+            {
+                HidePanel();
+            }
+            else
+            {
+                ShowPanel();
+            }
         }
 
-        public void PacientReady() => Pacient.Loaded.HowToPlayDone = true;
+        public void SwitchImage()
+        {
+            if (iterator == imageList.Length - 1)
+                return;
+
+            iterator++;
+
+            if (iterator == imageList.Length - 1)
+                this.GetComponentInChildren<Button>().onClick.AddListener(PacientReady);
+
+            image.sprite = imageList[iterator];
+        }
+
+        private void PacientReady()
+        {
+            Pacient.Loaded.HowToPlayDone = true;
+            ResetPanel();
+        }
+
+        private void ResetPanel()
+        {
+            iterator = 0;
+            image.sprite = imageList[iterator];
+            this.GetComponentInChildren<Button>().onClick.RemoveListener(PacientReady);
+            HidePanel();
+        }
+
+        private void ShowPanel() => this.transform.localScale = Vector3.one;
+        private void HidePanel() => this.transform.localScale = Vector3.zero;
     }
 }
