88ccf03a7eec58e3ba8898330cae2375360c0a6c
Renato Grimes <huenato@users.noreply.github.com>
2018-04-16 16:42:00 -0300

    Revamp UI

---
+++ Assets/MainMenuWallpaper.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class MainMenuWallpaper : MonoBehaviour
+{
+    private Vector2 _offset;
+    private MeshRenderer _mr;
+    private Renderer bgRenderer;
+
+    [SerializeField]
+    private bool scroll;
+
+    [SerializeField]
+    private float scrollSpeed = 0.1f;
+
+    [SerializeField]
+    private Material day;
+
+    [SerializeField]
+    private Material afternoon;
+
+    private void Awake()
+    {
+        bgRenderer = this.GetComponent<Renderer>();
+        _mr = this.GetComponent<MeshRenderer>();
+    }
+
+    private void Start() => SwitchBackground();
+
+    private void SwitchBackground()
+    {
+        switch (Random.Range(0,2))
+        {
+            default:
+            case 0:
+                bgRenderer.material = day;
+                break;
+
+            case 1:
+                bgRenderer.material = afternoon;
+                break;
+        }
+    }
+
+    private void Update() => Scroll();
+
+    private void Scroll()
+    {
+        _offset = _mr.material.mainTextureOffset;
+        _offset.x += Time.deltaTime / (1f / scrollSpeed);
+        _mr.material.mainTextureOffset = _offset;
+    }
+}
