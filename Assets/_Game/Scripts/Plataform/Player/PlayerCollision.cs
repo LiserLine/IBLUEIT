cb80fa03bef2760eae01a21df4126bb663f2df2e
Renato Grimes <huenato@users.noreply.github.com>
2018-04-24 04:22:33 -0300

    Colisão Golfinho

--- Assets/_Game/Scripts/Plataform/Player/PlayerCollision.cs
+++ Assets/_Game/Scripts/Plataform/Player/PlayerCollision.cs
@@ -44,10 +44,20 @@ namespace Ibit.Plataform
             }
         }
 
+        /*  Somehow, the animation method is generating an 
+            "Assertion Failed IsNormalized" kinda of an error.
+            At the moment, DamageAnimation() is using a band-aid fix,
+            to trick the player that it is playing an animation.
+            Use the band-aid method until the correct method is fixed. */
+        /// <summary>
+        /// Disables Collision and plays invincible animation.
+        /// </summary>
+        /// <param name="i"></param>
+        /// <returns></returns>
         private IEnumerator DisableCollisionForXSeconds(int i)
         {
             isInvulnerable = true;
-            animator.SetBool("DamageTaken", isInvulnerable);
+            //animator.SetBool("DamageTaken", isInvulnerable);
 
             var component = this.GetComponent<Collider2D>();
 
@@ -56,8 +66,36 @@ namespace Ibit.Plataform
             component.enabled = true;
 
             isInvulnerable = false;
-            animator.SetBool("DamageTaken", isInvulnerable);
+            //animator.SetBool("DamageTaken", isInvulnerable);
         }
+
+        /// <summary>
+        /// Band-aid fix. Check DisableCollisionForXSeconds() description.
+        /// </summary>
+        /// <returns></returns>
+        private IEnumerator DamageAnimation(float duration)
+        {
+            var delta = 0f;
+            var renderer = this.GetComponentInChildren<SpriteRenderer>();
+            var delay = 0.2f;
+
+            animator.enabled = false;
+
+            while (delta <= duration)
+            {
+                renderer.enabled = !renderer.enabled;
+                yield return new WaitForSeconds(delay);
+                delta += delay;
+            }
+
+            renderer.enabled = true;
+            animator.enabled = true;
+        }
+
+        /// <summary>
+        /// Band-aid fix. Check DisableCollisionForXSeconds() description.
+        /// </summary>
+        private Coroutine _animRoutine;
 
         [Button("Take Damage")]
         private void TakeDamage()
@@ -66,6 +104,16 @@ namespace Ibit.Plataform
                 return;
 
             StartCoroutine(DisableCollisionForXSeconds(invincibilityTime));
+
+            // Band-aid fix. Remove once it's fixed.
+            #region Damage Animation Band-Aid Fix.
+
+            if (_animRoutine != null)
+                StopCoroutine(_animRoutine);
+
+            _animRoutine = StartCoroutine(DamageAnimation(invincibilityTime));
+
+            #endregion Damage Animation Band-Aid Fix.
 
             heartPoints--;
 
