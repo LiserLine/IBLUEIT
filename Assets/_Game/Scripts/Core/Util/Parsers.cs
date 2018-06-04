b5290b618a70bcd4a17caccd5153806d8ed1a514
Renato Grimes <huenato@users.noreply.github.com>
2018-06-04 16:35:20 -0300

    Novo Parse Float

--- Assets/_Game/Scripts/Core/Util/Parsers.cs
+++ Assets/_Game/Scripts/Core/Util/Parsers.cs
@@ -2,6 +2,7 @@
 {
     public class Parsers
     {
-        public static float Float(string value) => float.Parse(value.Replace(',', '.'));
+        public static float Float(string value) => float.Parse(value.Replace(',', '.'),
+            System.Globalization.CultureInfo.InvariantCulture);
     }
 }
