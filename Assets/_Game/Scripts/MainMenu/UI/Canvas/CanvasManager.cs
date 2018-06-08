cce2f3b717ad69c7804438c3388eccf9371c0003
Renato Grimes <huenato@users.noreply.github.com>
2018-06-08 00:42:59 -0300

    Freq Resp Média Display

--- Assets/_Game/Scripts/MainMenu/UI/Canvas/CanvasManager.cs
+++ Assets/_Game/Scripts/MainMenu/UI/Canvas/CanvasManager.cs
@@ -8,39 +8,39 @@ namespace Ibit.MainMenu.UI.Canvas
 {
     public partial class CanvasManager : MonoBehaviour
     {
-        public void ShowPlayerInfo()
+        public void ShowPlayerInfo ()
         {
-            SysMessage.Info($"Jogador: {Pacient.Loaded.Name}\n" +
-                            $"Condição: {Pacient.Loaded.Condition}\n" +
-                            $"Partidas Jogadas: {Pacient.Loaded.PlaySessionsDone}\n" +
-                            $"Pico Exp.: {FlowMath.ToLitresPerMinute(Pacient.Loaded.Capacities.RawExpPeakFlow)} L/min ({Pacient.Loaded.Capacities.RawExpPeakFlow} Pa)\n" +
-                            $"Pico Ins.: {FlowMath.ToLitresPerMinute(Pacient.Loaded.Capacities.RawInsPeakFlow)} L/min ({Pacient.Loaded.Capacities.RawInsPeakFlow} Pa)\n" +
-                            $"Tempo Ins.: {Pacient.Loaded.Capacities.RawInsFlowDuration / 1000f:F1} s\n" +
-                            $"Tempo Exp.: {Pacient.Loaded.Capacities.RawExpFlowDuration / 1000f:F1} s\n" +
-                            $"Tins/Texp: {((Pacient.Loaded.Capacities.RawInsFlowDuration / 1000f) / (Pacient.Loaded.Capacities.RawExpFlowDuration / 1000f)):F1}\n" +
-                            $"Freq. Resp. Média: {Pacient.Loaded.Capacities.RawRespCycleDuration / 1000f:F1} sec/cycle");
+            SysMessage.Info ($"Jogador: {Pacient.Loaded.Name}\n" +
+                $"Condição: {Pacient.Loaded.Condition}\n" +
+                $"Partidas Jogadas: {Pacient.Loaded.PlaySessionsDone}\n" +
+                $"Pico Exp.: {FlowMath.ToLitresPerMinute(Pacient.Loaded.Capacities.RawExpPeakFlow)} L/min ({Pacient.Loaded.Capacities.RawExpPeakFlow} Pa)\n" +
+                $"Pico Ins.: {FlowMath.ToLitresPerMinute(Pacient.Loaded.Capacities.RawInsPeakFlow)} L/min ({Pacient.Loaded.Capacities.RawInsPeakFlow} Pa)\n" +
+                $"Tempo Ins.: {Pacient.Loaded.Capacities.RawInsFlowDuration / 1000f:F1} s\n" +
+                $"Tempo Exp.: {Pacient.Loaded.Capacities.RawExpFlowDuration / 1000f:F1} s\n" +
+                $"Tins/Texp: {((Pacient.Loaded.Capacities.RawInsFlowDuration / 1000f) / (Pacient.Loaded.Capacities.RawExpFlowDuration / 1000f)):F1}\n" +
+                $"Freq. Resp. Média: {Pacient.Loaded.Capacities.RawRespRate * 60f:F1} rpm");
         }
 
-        private void AddClickSfxToButtons()
-        {
-            foreach (var component in GameObject.Find("Canvas").GetComponentsInChildren(typeof(Button), true))
-            {
-                var btn = (Button)component;
-                btn.onClick.AddListener(PlayClick);
-            }
-        }
-
-        private void Awake()
+        private void Awake ()
         {
             if (Pacient.Loaded == null)
                 return;
 
-            GameObject.Find("Canvas").transform.Find("Start Panel").gameObject.SetActive(false);
-            GameObject.Find("Canvas").transform.Find("Player Menu").gameObject.SetActive(true);
+            GameObject.Find ("Canvas").transform.Find ("Start Panel").gameObject.SetActive (false);
+            GameObject.Find ("Canvas").transform.Find ("Player Menu").gameObject.SetActive (true);
         }
 
-        private void PlayClick() => SoundManager.Instance.PlaySound("BtnClickUI");
+        private void PlayClick () => SoundManager.Instance.PlaySound ("BtnClickUI");
 
-        private void Start() => AddClickSfxToButtons();
+        private void Start () => AddClickSfxToButtons ();
+
+        private void AddClickSfxToButtons ()
+        {
+            foreach (var component in GameObject.Find ("Canvas").GetComponentsInChildren (typeof (Button), true))
+            {
+                var btn = (Button) component;
+                btn.onClick.AddListener (PlayClick);
+            }
+        }
     }
 }
